6eea3d9d9aaab6188e7ece6f8950cb21e6d87b1c
nuoo1125 <touma201125@gmail.oom>
2025-01-24 00:28:23 +0900

    upload

--- sendai_pico/main/main.cpp
+++ sendai_pico/main/main.cpp
@@ -9,9 +9,34 @@
 
 int main() {
     stdio_init_all();
+    /*
     init_bno055();
     int16_t yaw, roll, pitch;
     stepper_setup();
     stepper_slow(0,0);
+    */
+    gpio_set_dir(6,1);
+    
+    while(1){
+        for(int i=0;i<8;i++){
+            gpio_put(6,1);
+            sleep_us(1.36);
+            gpio_put(6,0);
+            sleep_us(0.35);
+        }
+        for(int i=0;i<8;i++){
+            gpio_put(6,1);
+            sleep_us(0.35);
+            gpio_put(6,0);
+            sleep_us(1.36);
+        }
+        for(int i=0;i<8;i++){
+            gpio_put(6,1);
+            sleep_us(0.35);
+            gpio_put(6,0);
+            sleep_us(1.36);
+        }
+        sleep_us(50);
+    }
     return 0;
 }
