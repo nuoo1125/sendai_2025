4c004736bdc27142cde5c24b7b9d89c5da7a79b0
nuoo1125 <touma201125@gmail.com>
2025-02-03 00:21:41 +0900

    pl9823finish

--- sendai_pico/main/main.cpp
+++ sendai_pico/main/main.cpp
@@ -6,37 +6,37 @@
 #include "servo/servo.h"
 #include "mcp3208/mcp3208.h"
 #include "stepper/stepper.h"
+#include "ws2812.pio.h"
+
 
 int main() {
     stdio_init_all();
+    sleep_ms(2000);
     /*
     init_bno055();
     int16_t yaw, roll, pitch;
     stepper_setup();
     stepper_slow(0,0);
     */
-    gpio_set_dir(6,1);
-    
-    while(1){
-        for(int i=0;i<8;i++){
-            gpio_put(6,1);
-            sleep_us(1.36);
-            gpio_put(6,0);
-            sleep_us(0.35);
-        }
-        for(int i=0;i<8;i++){
-            gpio_put(6,1);
-            sleep_us(0.35);
-            gpio_put(6,0);
-            sleep_us(1.36);
-        }
-        for(int i=0;i<8;i++){
-            gpio_put(6,1);
-            sleep_us(0.35);
-            gpio_put(6,0);
-            sleep_us(1.36);
-        }
-        sleep_us(50);
+    PIO pio = pio0;
+    uint sm = 0;
+    uint offset = pio_add_program(pio, &ws2812_program);
+    ws2812_program_init(pio, sm, offset, WS2812_PIN, 800000, IS_RGBW);
+
+    while (true) {
+        set_pixel_color(pio, sm, 125, 0, 0);  
+        sleep_ms(1000);
+
+        set_pixel_color(pio, sm, 0, 125, 0);  
+        sleep_ms(1000);
+
+        set_pixel_color(pio, sm, 0, 0, 125);  
+        sleep_ms(1000);
+
+        set_pixel_color(pio, sm, 255, 255, 255);  
+        sleep_ms(1000);
+
+        set_pixel_color(pio, sm, 0, 0, 0); 
+        sleep_ms(1000);
     }
-    return 0;
 }
